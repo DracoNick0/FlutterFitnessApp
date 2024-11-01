9890da62810cfd89db88d5ad1d7ca136395fc042
Nicholas Santos <santos_nick@outlook.com>
2024-11-01 08:11:04 -0700

    Added suffox icon in search bar

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -26,7 +26,14 @@ class HomePage extends StatelessWidget {
                 filled: true,
                 fillColor: Colors.white,
                 contentPadding: const EdgeInsets.all(15),
-                prefixIcon: SvgPicture.asset('assets/icons/Search.svg'),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: SvgPicture.asset('assets/icons/Search.svg'),
+                ),
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: SvgPicture.asset('assets/icons/Filter.svg'),
+                ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(15),
                   borderSide: BorderSide.none,
